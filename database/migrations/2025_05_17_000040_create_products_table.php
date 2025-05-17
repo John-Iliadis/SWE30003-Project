a0b9ef9c670e908cdd0c2f3b6a6760b54b92c171
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2025-05-17 13:28:05 +1000

    created database migrations

---
+++ database/migrations/2025_05_17_000040_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('product_id');
+            $table->string('name');
+            $table->string('brand');
+            $table->string('description');
+            $table->decimal('price');
+            $table->integer('quantity');
+            $table->foreignId('category_id')->nullable()->references('category_id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2025_05_17_000070_create_orderline_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orderline', function (Blueprint $table) {
+            $table->bigIncrements('orderline_id');
+            $table->foreignId('order_id')->nullable()->references('order_id')->on('orders');
+            $table->foreignId('product_id')->nullable()->references('product_id')->on('products');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orderline');
+    }
+};
