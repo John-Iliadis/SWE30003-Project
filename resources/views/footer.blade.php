e8584e80210abe61b5355793e90462b49a6881af
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2025-05-20 12:21:31 +1000

    added footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,19 @@
+<!-- Basic footer information included on all pages. All its contents are false and not true. -->
+
+<footer>
+
+    <img src="{{asset('img/app/logo_white.png')}}" alt="Store Logo">
+
+    <nav id="footer_nav">
+        <p class="footer_menu"><a href="#">Privacy Policy</a></p>
+        <p class="footer_menu"><a href="#">Careers</a> </p>
+        <p class="footer_menu"><a href="#">Locations</a></p>
+        <p class="footer_menu"><a href="#">Contact</a></p>
+        <p class="footer_menu"><a href="#">Rewards Club</a></p>
+    </nav>
+
+    <p id="copyright">&#169 2022 AWE Electronics Corporation</p>
+
+    <p id="notice">This website and its content is copyright of AWE Electronics, all rights reserved. Any redistribution or reproduction of part or all of the contents in any form is prohibited.</p>
+
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home</title>
 
+    <link rel="stylesheet" href="{{asset('css/global.css')}}">
     <link rel="stylesheet" href="{{asset('css/header.css')}}">
+    <link rel="stylesheet" href="{{asset('css/footer.css')}}">
+
 </head>
 <body>
 
     @include('header')
+    @include('footer')
 
 </body>
 </html>
