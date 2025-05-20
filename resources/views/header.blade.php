4474e840cc43f2b987414fc47d06363b9a2fbb3f
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2025-05-20 11:56:01 +1000

    added header

---
+++ resources/views/header.blade.php
@@ -0,0 +1,15 @@
+<!-- Top header that consists of the shops logo and navigation links. This is included on all pages -->
+
+<header>
+
+    <img src="{{asset('img/app/logo.png')}}" alt="Store Logo">
+
+    <nav id="header_nav">
+        <p class="menu"><a href="#">Home</a></p>
+        <p class="menu"><a href="#">Account</a></p>
+        <p class="menu"><a href="#">Products</a></p>
+        <p class="menu"><a href="#">Cart</a></p>
+        <p class="menu"><a href="#">Admin</a></p>
+    </nav>
+
+</header>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+
+    <link rel="stylesheet" href="{{asset('css/header.css')}}">
+</head>
+<body>
+
+    @include('header')
+
+</body>
+</html>
