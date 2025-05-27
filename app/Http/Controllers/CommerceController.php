08864aa66a14e2fd75506c57c6465c5452392cab
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2025-05-27 18:22:23 +1000

    added catalogue filter functionality for radios and checkboxes

--- app/Http/Controllers/CommerceController.php
+++ app/Http/Controllers/CommerceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
+use Exception;
 use Illuminate\Http\Request;
 
 // todo: product may not exist
@@ -12,7 +13,7 @@ class CommerceController
     public function catalogue()
     {
         $data = [
-            'products' => Product::orderBy('brand', 'ASC')->get(),
+            'products' => Product::orderBy('brand')->orderBy('name')->get(),
             'categories' => Category::all(),
             'brands' => Product::getBrands(),
             'price_ranges' => Product::getPriceRanges()
@@ -33,5 +34,59 @@ class CommerceController
     public function cart()
     {
         return view('cart');
+    }
+
+    public function filter(Request $request)
+    {
+        $data = $request->all();
+
+        $sort = $data['sort'];
+        $categories = $data['category'] ?? null;
+        $brands = $data['brand'] ?? null;
+        $price_ranges = $data['price_range'] ?? null;
+
+        $products = Product::query();
+
+        if ($categories)
+        {
+            $products->whereIn('category_name', $categories);
+        }
+
+        if ($brands)
+        {
+            $products->whereIn('brand', $brands);
+        }
+
+        if ($price_ranges)
+        {
+            $products->where(function ($query) use ($price_ranges)
+            {
+                foreach ($price_ranges as $range)
+                {
+                    [$min, $max] = explode(' ', $range);
+                    $query->orWhereBetween('price', [(float)$min, (float)$max]);
+                }
+            });
+        }
+
+        switch ($sort)
+        {
+            case 'alphabetical_ascending':
+                $products->orderBy('brand')->orderBy('name');
+                break;
+            case 'alphabetical_descending':
+                $products->orderBy('brand', 'desc')->orderBy('name', 'desc');
+                break;
+            case 'price_ascending':
+                $products->orderBy('price');
+                break;
+            case 'price_descending':
+                $products->orderBy('price', 'desc');
+                break;
+        }
+
+        $products = $products->get();
+
+        return view('catalogue_items', ['products' => $products]);
     }
 }
---
+++ public/js/catalogue_filter.js
@@ -0,0 +1,64 @@
+function getFilterData()
+{
+    const filterData = {};
+
+    const radioData = document.querySelectorAll('#filter input[type="radio"]');
+    radioData.forEach(radio => {
+        if (radio.checked)
+        {
+            filterData[radio.name] = radio.value;
+        }
+    });
+
+    const checkBoxData = document.querySelectorAll('#filter input[type="checkbox"]');
+    checkBoxData.forEach(checkbox => {
+        if (checkbox.checked)
+        {
+            if (!filterData[checkbox.name])
+            {
+                filterData[checkbox.name] = [];
+            }
+
+            filterData[checkbox.name].push(checkbox.value);
+        }
+    });
+
+    return filterData;
+}
+
+function updateCatalogue()
+{
+    const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('post', '/catalogue/filter', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('X-CSRF-TOKEN', token);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4)
+        {
+            if (xhr.status === 200)
+            {
+                document.getElementById('catalogue').innerHTML = xhr.responseText;
+            }
+            else
+            {
+                console.error('Error:', xhr.statusText);
+            }
+        }
+    }
+
+    xhr.send(JSON.stringify(getFilterData()));
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let filterVals = document.getElementsByClassName('filter_val');
+
+    for (let filterVal of filterVals)
+    {
+        filterVal.addEventListener('change', updateCatalogue);
+    }
+});
