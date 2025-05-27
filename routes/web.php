b95b3a40231357d302cb8d597caffc6f8b014b7c
ahnaf-tajwar <126742264+ahnaf9094@users.noreply.github.com>
2025-05-27 17:59:43 +1000

    refactor(routes): simplify route definitions and add admin auth middleware
    - Consolidate duplicate home route definitions into single-line syntax
    - Replace individual product routes with resourceful routing
    - Add auth and admin middleware to admin routes for security

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
-use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CommerceController;
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/', function () { return view('home'); });
+Route::get('/home', function () { return view('home'); });
 
 Route::get('/login', function () {
     return view('login');
@@ -29,11 +25,6 @@ Route::post('continue', function() {
     return view('register_details');
 });
 
-Route::prefix('admin')->group(function () {
-    Route::get('/products', [App\Http\Controllers\ProductController::class, 'index']);
-    Route::get('/products/create', [App\Http\Controllers\ProductController::class, 'create']);
-    Route::post('/products', [App\Http\Controllers\ProductController::class, 'store']);
-    Route::get('/products/{product}/edit', [App\Http\Controllers\ProductController::class, 'edit']);
-    Route::put('/products/{product}', [App\Http\Controllers\ProductController::class, 'update']);
-    Route::delete('/products/{product}', [App\Http\Controllers\ProductController::class, 'destroy']);
+Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () {
+    Route::resource('products', ProductController::class)->except(['show']);
 });
