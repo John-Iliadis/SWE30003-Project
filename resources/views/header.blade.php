9b8cd96986718df75b8a4caf0ce4156de8219208
ahnaf-tajwar <126742264+ahnaf9094@users.noreply.github.com>
2025-05-27 18:06:12 +1000

    fix(views): update admin link to use named route
    The admin link was previously a dead link (#). Changed it to use the named route 'admin.products.index' to properly direct to the admin products page.

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -11,7 +11,7 @@
         <p class="menu"><a href="/login">Account</a></p>
         <p class="menu"><a href="/catalogue">Catalogue</a></p>
         <p class="menu"><a href="/cart">Cart</a></p>
-        <p class="menu"><a href="#">Admin</a></p>
+        <p class="menu"><a href="{{ route('admin.products.index') }}">Admin</a></p>
     </nav>
 
 </header>
