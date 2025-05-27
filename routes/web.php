c571221c17450e448d6e6ab5fee57763f0d2e586
ahnaf-tajwar <126742264+ahnaf9094@users.noreply.github.com>
2025-05-27 17:54:56 +1000

    feat(admin): add product management routes
    Add CRUD routes for product management under admin prefix to enable product operations

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,12 @@ Route::post('/login', [AccountController::class, 'login']);
 Route::post('continue', function() {
     return view('register_details');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/products', [App\Http\Controllers\ProductController::class, 'index']);
+    Route::get('/products/create', [App\Http\Controllers\ProductController::class, 'create']);
+    Route::post('/products', [App\Http\Controllers\ProductController::class, 'store']);
+    Route::get('/products/{product}/edit', [App\Http\Controllers\ProductController::class, 'edit']);
+    Route::put('/products/{product}', [App\Http\Controllers\ProductController::class, 'update']);
+    Route::delete('/products/{product}', [App\Http\Controllers\ProductController::class, 'destroy']);
+});
