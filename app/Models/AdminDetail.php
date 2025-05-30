84560e12ed28abb454d20660f81fef5654d9fa02
ahnaf-tajwar <126742264+ahnaf9094@users.noreply.github.com>
2025-05-31 07:19:12 +1000

    refactor(AdminDetail): convert model to use Authenticatable
    The AdminDetail model was previously extending Model directly. Changed it to extend Authenticatable to enable authentication features. Added necessary traits (HasFactory, Notifiable) and implemented standard authentication properties (fillable, hidden, casts) to support Laravel's authentication system.

--- app/Models/AdminDetail.php
+++ app/Models/AdminDetail.php
@@ -2,9 +2,49 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable; // Important for authentication
+use Illuminate\Notifications\Notifiable;
 
-class AdminDetail extends Model
+class AdminDetail extends Authenticatable // Extend Authenticatable
 {
-    //
+    use HasFactory, Notifiable;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'admin_details';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
