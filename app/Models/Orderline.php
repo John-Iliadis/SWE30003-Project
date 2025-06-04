ebb37a156d98387bec278e10e605bcf49706e441
ahnaf-tajwar <126742264+ahnaf9094@users.noreply.github.com>
2025-06-05 09:06:26 +1000

    feat(Orderline): add order and product relationship methods
    Add belongsTo relationships to Orderline model for Order and Product

--- app/Models/Orderline.php
+++ app/Models/Orderline.php
@@ -14,4 +14,14 @@ class Orderline extends Model
         'product_id',
         'quantity',
     ];
+    
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id', 'order_id');
+    }
+    
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'product_id');
+    }
 }
