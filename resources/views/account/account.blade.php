48dc2c047d778642ca4b8eefb2166573f6646278
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2025-06-05 22:45:15 +1000

    modified account styles

--- resources/views/account/account.blade.php
+++ resources/views/account/account.blade.php
@@ -20,7 +20,7 @@
 
     <main>
         <div class="account-container">
-            <div style="display: flex; justify-content: space-between; align-items: center;">
+            <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 30px">
                 <h1 id="title">My Account</h1>
                 <a href="/order-history" class="edit-btn">View Order History</a>
             </div>
